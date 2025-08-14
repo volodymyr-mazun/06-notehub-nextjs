473fd19eff2762f48e2380c0a4e8bdc7df981ea2
Volodymyr Mazun <mebelmv1990@gmail.com>
2025-08-14 07:47:43 +0300

    add file loading

--- app/notes/[id]/error.tsx
+++ app/notes/[id]/error.tsx
@@ -1,19 +1,4 @@
-
-// ----------КОМПОНЕНТ, ГЛОБАЛЬНИЙ ERROR----------
-
 "use client";
-
-type Props = {
-  error: Error;
-  reset: () => void;
-};
-
-export default function Error({ error, reset }: Props) {
-  return (
-    <div>
-      <h2>Помилка при завантаженні</h2>
-      <p>Could not fetch the list of notes. {error.message}</p>
-      <button onClick={reset}>Спробувати знову</button>
-    </div>
-  );
+export default function Error({ error }: { error: Error }) {
+  return <p>Could not fetch note details. {error.message}</p>;
 }
